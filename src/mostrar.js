2f6d1b1042d604b046a571b6411098ee34d2529b
Aldrin <axzt2004@gmail.com>
2025-03-10 16:22:41 -0400

    fix: calcular costo envio 41 - 80 con funcionalidad

--- src/mostrar.js
+++ src/mostrar.js
@@ -100,7 +100,7 @@ function calcularCostoDeEnvio(pesoVolumetrico){
     const costoDeEnvio = [
         {min: 11, max: 20, costo: 3.5},
         {min: 21, max: 40, costo: 5},
-        {min: 41, max: 80, costo: 6},
+        {min: 41, max: 80, costo: 6 },
     ];
     return costoDeEnvio.find(t => pesoVolumetrico >= t.min && pesoVolumetrico <= t.max)?.costo || 0;
 }
